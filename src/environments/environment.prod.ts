7786b8413dfc4e7c59595f54ab0330e2136f7508
zhaozhongbo <zzb8110@163.com>
2018-08-30 16:05:37 +0800

    修改请求ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   pid: 'BUDGET',
   projectName: '全面预算报表',
-  systemurl: 'http://192.168.0.195:8080/server/',
-  apiurl: 'http://192.168.0.195:8080/api/',
-  logurl: 'http://192.168.0.195:8080/api/',
-  authurl: 'http://192.168.0.195:8080/api/',
-  wsurl: 'ws://192.168.0.195:8080/server/msg',
+  systemurl: 'http://62.234.178.106:8080/server/',
+  apiurl: 'http://62.234.178.106:8080/api/',
+  logurl: 'http://62.234.178.106:8080/api/',
+  authurl: 'http://62.234.178.106:8080/api/',
+  wsurl: 'ws://62.234.178.106:8080/server/msg',
   license: ''
 };
